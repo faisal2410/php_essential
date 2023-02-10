074e49af7e04266a5376d991793c59bb89bd7a82
Faisal ahmed <faisal2410@yahoo.com>
2023-02-10 09:16:57 +0600

    updated

--- operators.php
+++ operators.php
@@ -9,32 +9,45 @@
 <body>
 
     <?php
-      $a = 4;
-      $b = 3;
-      $c = 1;
-      $d = 20;
-      if (($a >= $b) || ($c >= $d)) {
-        echo "a is larger than b OR ";
-        echo "c is larger than d";
-      }
-    ?>
+$a = 4;
+$b = 3;
+$c = 1;
+$d = 20;
+if (($a >= $b) || ($c >= $d)) {
+    echo "a is larger than b OR ";
+    echo "c is larger than d";
+}
+?>
     <br />
     <?php
-      $e = 100;
-      if (!isset($e)) {
-        $e = 200;
-      }
-      echo $e;
-    ?>
+
+/*
+The isset() function checks whether a variable is set, which means that it has to be declared and is not NULL. This function returns true if the variable exists and is not NULL, otherwise it returns false.
+ */
+$e = 100;
+if (!isset($e)) {
+    $e = 200;
+}
+echo $e;
+?>
     <br />
     <?php
-      // don't reject 0 or 0.0
-      $quantity = "";
-      if (empty($quantity) && !is_numeric($quantity)) {
-        echo "You must enter a quantity.";
-      }
-    ?>
-    
-    
+// don't reject 0 or 0.0
+/*
+The empty() function checks whether a variable is empty or not.
+
+This function returns false if the variable exists and is not empty, otherwise it returns true.
+
+The following values evaluates to empty:0, 0.0, "0", "", NULL, FALSE, array()
+=============================================================================
+
+ */
+$quantity = "";
+if (empty($quantity) && !is_numeric($quantity)) {
+    echo "You must enter a quantity.";
+}
+?>
+
+
   </body>
 </html>
